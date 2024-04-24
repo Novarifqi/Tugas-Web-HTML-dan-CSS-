0909cadba2a6be56a369869d4cf7a87ddb35006b
Novarifqi <75981020+Novarifqi@users.noreply.github.com>
2024-04-25 05:26:57 +0700

    Delete Tugas HTML dan CSS/script.js

--- Tugas HTML dan CSS/script.js
+++
@@ -1,9 +0,0 @@
-// Simpan data acara (contoh: di localStorage)
-function simpanDataAcara(dataAcara) {
-  localStorage.setItem('dataAcara', JSON.stringify(dataAcara));
-}
-
-// Ambil data acara (contoh: dari localStorage)
-function ambilDataAcara() {
-  const dataAcara = JSON.parse(localStorage.getItem('dataAcara')) || [];
-}
